368e6e88ee400f571cd7e84166414f02065b3121
Izazkhan829 <sarbudeenizazsa@gmail.com>
2026-02-08 10:51:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 import subprocess
-import os
-import sys
+import sys, os
 
 app = Flask(__name__)
 
@@ -9,8 +8,8 @@ app = Flask(__name__)
 def home():
     return "Bot is running 🚀"
 
-# Start bot using the SAME python interpreter
-subprocess.Popen([sys.executable, "bot.py"])
+# Start the bot using the correct entrypoint
+subprocess.Popen([sys.executable, "main.py"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
